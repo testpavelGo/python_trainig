9f2bb64ed238eff2e1bfb5514a91a4f78b23f317
Pavel <p.gorozhankin@pravo.tech>
2022-03-28 00:02:06 +0400

    новый метод для редактирования первой группы

--- fixture/group.py
+++ fixture/group.py
@@ -12,7 +12,6 @@ class GroupHelper:
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
-        wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").clear()
@@ -30,6 +29,23 @@ class GroupHelper:
         wd.find_element_by_name('selected[]').click()
         # click 'Delete group(s)' button
         wd.find_element_by_name('delete').click()
+        self.return_to_groups_page()
+
+    def edit_first_group(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group checkbox
+        wd.find_element_by_name('selected[]').click()
+        # click 'Edit group' button
+        wd.find_element_by_name('edit').click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
+        wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
     def return_to_groups_page(self):
