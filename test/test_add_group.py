7ecb70d57262c879e3a728e5be77547b9058cd9a
testpavelGo <pavel1-go@inbox.ru>
2022-06-10 01:04:11 +0400

    новая соеда

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,7 +6,6 @@ def test_add_group(app):
     app.group.create(Group(name="nnn", header="opo", footer="1234"))
     app.session.logout()
 
-#only comment
 def test_add_group_empty_parameters(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
